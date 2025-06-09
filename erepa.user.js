f893a6da84809932e57d222edb5714032676f156
Yurii <driversti@gmail.com>
2025-06-09 18:38:25 +0200

    Updated to 0.0.4
    Implemented the DailyTasks feature.

--- erepa.user.js
+++ erepa.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name eRepa
-// @version 0.0.1
+// @version 0.0.4
 // @description An Uber-script aiming to cover all you might need in the game.
 // @namespace http://tampermonkey.net/
 // @author driversti (https://www.erepublik.com/en/citizen/profile/4690052)
