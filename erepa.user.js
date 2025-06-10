a3ab1ba9f6af4e56080d0412ddc44596f02be8bd
Yurii <driversti@gmail.com>
2025-06-10 07:00:45 +0200

    Updated to 0.0.5
    Improved the elapsed time indicator.

--- erepa.user.js
+++ erepa.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name eRepa
-// @version 0.0.4
+// @version 0.0.5
 // @description An Uber-script aiming to cover all you might need in the game.
 // @namespace http://tampermonkey.net/
 // @author driversti (https://www.erepublik.com/en/citizen/profile/4690052)
