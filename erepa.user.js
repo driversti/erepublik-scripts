33f8f2ea61c941dcd96530be6dfd639c7979ec2c
Yurii <driversti@gmail.com>
2025-06-13 18:50:48 +0200

    v0.0.12. Added collecting rewards

--- erepa.user.js
+++ erepa.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name eRepa
-// @version 0.0.11
+// @version 0.0.12
 // @description An Uber-script aiming to cover all you might need in the game.
 // @namespace http://tampermonkey.net/
 // @author driversti (https://www.erepublik.com/en/citizen/profile/4690052)
