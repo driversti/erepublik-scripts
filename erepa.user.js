b361e41529a911ddec27ce08b28832d52d23648d
Yurii <driversti@gmail.com>
2025-10-27 06:34:33 +0100

    v1.10.2
    minor improvements of post filtering
    increased time of auto farming

--- erepa.user.js
+++ erepa.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name eRepa
-// @version 1.10.1
+// @version 1.10.2
 // @description An Uber-script aiming to cover all you might need in the game.
 // @namespace https://github.com/driversti/erepublik-scripts
 // @author driversti (https://www.erepublik.com/en/citizen/profile/4690052)
